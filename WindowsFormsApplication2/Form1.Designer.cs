f0eeb0940013e762dcd55c67f8731f37a3afabae
Kwame <Baffawuah@yahoo.com>
2017-04-23 22:43:20 -0400

    Add project files.

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openPointsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ptDisplay = new System.Windows.Forms.ListBox();
+            this.clrDisplay = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.xBox = new System.Windows.Forms.TextBox();
+            this.yBox = new System.Windows.Forms.TextBox();
+            this.tBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTag = new System.Windows.Forms.Label();
+            this.pBox = new System.Windows.Forms.TextBox();
+            this.errTag = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openPointsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openPointsToolStripMenuItem
+            // 
+            this.openPointsToolStripMenuItem.Name = "openPointsToolStripMenuItem";
+            this.openPointsToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openPointsToolStripMenuItem.Text = "Open Points";
+            this.openPointsToolStripMenuItem.Click += new System.EventHandler(this.openPointsToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ptDisplay
+            // 
+            this.ptDisplay.FormattingEnabled = true;
+            this.ptDisplay.HorizontalScrollbar = true;
+            this.ptDisplay.Location = new System.Drawing.Point(12, 43);
+            this.ptDisplay.Name = "ptDisplay";
+            this.ptDisplay.Size = new System.Drawing.Size(396, 368);
+            this.ptDisplay.TabIndex = 2;
+            // 
+            // clrDisplay
+            // 
+            this.clrDisplay.Location = new System.Drawing.Point(162, 417);
+            this.clrDisplay.Name = "clrDisplay";
+            this.clrDisplay.Size = new System.Drawing.Size(83, 36);
+            this.clrDisplay.TabIndex = 3;
+            this.clrDisplay.Text = "Clear";
+            this.clrDisplay.UseVisualStyleBackColor = true;
+            this.clrDisplay.Click += new System.EventHandler(this.clrDisplay_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(594, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Interpolate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // xBox
+            // 
+            this.xBox.Location = new System.Drawing.Point(617, 85);
+            this.xBox.Name = "xBox";
+            this.xBox.Size = new System.Drawing.Size(100, 20);
+            this.xBox.TabIndex = 5;
+            // 
+            // yBox
+            // 
+            this.yBox.Location = new System.Drawing.Point(617, 111);
+            this.yBox.Name = "yBox";
+            this.yBox.Size = new System.Drawing.Size(100, 20);
+            this.yBox.TabIndex = 6;
+            // 
+            // tBox
+            // 
+            this.tBox.Location = new System.Drawing.Point(617, 137);
+            this.tBox.Name = "tBox";
+            this.tBox.Size = new System.Drawing.Size(100, 20);
+            this.tBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(447, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter X value:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter Y Value:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(447, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Enter a Date Value (1-364)";
+            // 
+            // txtTag
+            // 
+            this.txtTag.AutoSize = true;
+            this.txtTag.Location = new System.Drawing.Point(447, 171);
+            this.txtTag.Name = "txtTag";
+            this.txtTag.Size = new System.Drawing.Size(142, 13);
+            this.txtTag.TabIndex = 11;
+            this.txtTag.Text = "Enter a Power/Weight value";
+            this.txtTag.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pBox
+            // 
+            this.pBox.Location = new System.Drawing.Point(617, 164);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(100, 20);
+            this.pBox.TabIndex = 12;
+            this.pBox.Text = "2";
+            this.pBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // errTag
+            // 
+            this.errTag.AutoSize = true;
+            this.errTag.Location = new System.Drawing.Point(447, 362);
+            this.errTag.Name = "errTag";
+            this.errTag.Size = new System.Drawing.Size(0, 13);
+            this.errTag.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.errTag);
+            this.Controls.Add(this.pBox);
+            this.Controls.Add(this.txtTag);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBox);
+            this.Controls.Add(this.yBox);
+            this.Controls.Add(this.xBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.clrDisplay);
+            this.Controls.Add(this.ptDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openPointsToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox ptDisplay;
+        private System.Windows.Forms.Button clrDisplay;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox xBox;
+        private System.Windows.Forms.TextBox yBox;
+        private System.Windows.Forms.TextBox tBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtTag;
+        private System.Windows.Forms.TextBox pBox;
+        private System.Windows.Forms.Label errTag;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        string readOut = "";
+        string[] dataChain;
+        ArrayList dataList = new ArrayList();
+        public double globC = 0;
+        double solution = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class dataPt
+        {
+            double x, y, measure, timeInt;
+
+            public dataPt(double Nx, double Ny, double NtimeInt, double Nmeasure)
+            {
+                this.x = Nx;
+                this.y = Ny;
+                this.measure = Nmeasure;
+                this.timeInt = NtimeInt;
+            }
+
+            public String ToString()
+            {
+                return this.x + "   " + this.y + "   " + this.measure + "   " + this.timeInt;
+            }
+            
+            public double getX()
+            {
+                return this.x;
+            }
+
+            public double getY()
+            {
+                return this.y;
+            }
+            public double getTime()
+            {
+                return this.timeInt;
+            }
+            public double getMeasure()
+            {
+                return this.measure;
+            }
+
+
+        }
+
+        public class dataArray
+        {
+            public ArrayList list = new ArrayList();
+
+            public void add(dataPt entry)
+            {
+                list.Add(entry);
+            }
+
+            public void add(double Nx, double Ny, double NtimeInt, double Nmeasure)
+            {
+                dataPt temp = new dataPt(Nx, Ny, NtimeInt, Nmeasure);
+                add(temp);
+            }
+
+            public void delete(dataPt entry)
+            {
+                // foreach()
+            }
+        }
+
+
+        private void openPointsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text files|*.txt|All files|*.*";
+            openFileDialog1.Title = "Open the Captured Packets";
+            openFileDialog1.ShowDialog();
+
+            //Check to see if a filename was given
+
+            if (openFileDialog1.FileName != "")
+
+                try
+                {
+                    {
+                        readOut = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                        //textBox1.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                        dataChain = readOut.Split(new String[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+                        //Read out Code
+                        string[] link; //dataChain[0].Split(null);
+
+
+
+                        for (int i = 0; i < dataChain.Length - 1; i++)
+                        {
+                            link = dataChain[i].Split(null);
+                            //textBox1.AppendText(link[0] + "         " + link[1] + "         " + link[2] + "         "+ link[3] + "\r\n");
+                            dataPt Temp = new dataPt(Convert.ToDouble(link[0]), Convert.ToDouble(link[1]), Convert.ToDouble(link[2]), Convert.ToDouble(link[3]));
+
+                            dataList.Add(Temp);
+                            //ptDisplay.Items.Add(Temp.ToString());
+
+                        }
+                    }
+                    ptDisplay.Items.Add(" Data Loading: Finished");
+                    button2.Enabled = true;
+                    globC = findC(dataList);
+                    dataPt temp = (dataPt)dataList[dataList.Count - 1];
+                    ptDisplay.Items.Add("--------------------------------------");
+                    ptDisplay.Items.Add("The Maximum X value of this list is: " + findMaxX(dataList));
+                    ptDisplay.Items.Add("The Minimum X value of this list is: " + findMinX(dataList));
+                    ptDisplay.Items.Add("The Maximum Y value of this list is: " + findMaxY(dataList));
+                    ptDisplay.Items.Add("the Minimum Y value of this list is: " + findMinY(dataList));
+                    ptDisplay.Items.Add("The C for this List is " + findC(dataList));
+                    //ptDisplay.Items.Add("A sample distance for the Test Data is " + findD((dataPt)dataList[0],(dataPt)dataList[1],globC));
+                    //ptDisplay.Items.Add("The Value for the test data is " + IDWE(-80,26,359,2));
+                    errTag.Text = "";
+                }
+                catch
+                {
+                    errTag.Text = "The file format you attempted to load is incorrect";
+                    errTag.ForeColor = Color.Red;
+                }
+
+
+        }
+
+        public ArrayList constructList(double v)
+        {
+            ArrayList parsedList = new ArrayList();
+            int comp1 = (int)(v-2);
+            int comp2;
+
+            for(int i = 0; i < dataList.Count; i++)
+            {
+                comp2 =  (int)((dataPt)dataList[i]).getX();
+                if (comp1 == comp2)
+                {
+                    parsedList.Add((dataPt)dataList[i]);
+                }
+            }
+
+
+
+
+            return parsedList;
+        }
+
+
+        public double IDWE(double x, double y, double t, double P, ArrayList points)
+        {
+            double Answer = 0;
+            dataPt holder = new dataPt(x,y,t,0);
+            double d1 = 0;
+            double d2 = 0;
+            double w = 0;
+            double top = 0;
+            double bottom = 0;
+
+
+            for(int i = 0; i <points.Count; i++)
+            {
+                d1 = findD( (dataPt) points[i] , holder , globC );
+                d2 = findD( (dataPt) points[i] , holder , globC );
+                w = ((dataPt)points[i]).getMeasure();
+
+                top += w / (Math.Pow(d1,P));
+                bottom += 1 / (Math.Pow(d2,P));
+
+
+
+            }
+
+            Answer = top / bottom;
+
+            return Answer;
+        }
+
+
+
+
+        //Lesser functions that add up to form the larger one to make the code more modular
+        //Check math sequence:
+
+        public double findD(dataPt x1, dataPt x, double C)
+        {
+            double d = 0;
+
+            d = Math.Sqrt(  (Math.Pow((x1.getX()- x.getX()),2.0)) + (Math.Pow((x1.getY() - x.getY()), 2)) + ( Math.Pow(globC,2.0) * (Math.Pow((x1.getTime() - x.getTime()), 2.0)))  );
+
+            return d;
+        }
+
+   
+        //SECOND VARIATION OF FINDING D
+
+
+
+
+        // Currently Under construction
+        public double findC(ArrayList listing)
+        {
+            double C;
+            double maxX = findMaxX(listing);
+            double minX = findMinX(listing);
+            double maxY = findMaxY(listing);
+            double minY = findMinY(listing);
+
+            C = (1.0 / 364.0)*(((maxX-minX)+(maxY-minY))/2);
+
+            return C;
+        }
+
+
+        //Currently Under construction
+        //Needs to be tested [X]
+        public double findMaxX(ArrayList listing)
+        {
+           double max = 0;
+            max = ((dataPt)listing[0]).getX();
+            for(int i = 0; i < listing.Count; i++)
+            {
+                if(max < ((dataPt)listing[i]).getX())
+                {
+                    max = ((dataPt)listing[i]).getX();
+                }
+            }
+            return max;
+        }
+        //Currently Under Construction
+        //Needs to be tested [X]
+        public double findMinX(ArrayList listing)
+        {
+            double min = 0;
+
+            min = ((dataPt)listing[0]).getX();
+
+            for (int i = 0; i < listing.Count; i++)
+            {
+                if (min > ((dataPt)listing[i]).getX())
+                {
+                    min = ((dataPt)listing[i]).getX();
+                }
+            }
+            return min;
+        }
+
+        //CONVERSION TO Y VERSION OF MAX FUNCTIONS
+        public double findMaxY(ArrayList listing)
+        {
+            double max = 0;
+            max = ((dataPt)listing[0]).getY();
+            for (int i = 0; i < listing.Count; i++)
+            {
+                if (max < ((dataPt)listing[i]).getY())
+                {
+                    max = ((dataPt)listing[i]).getY();
+                }
+            }
+            return max;
+        }
+        //Currently Under Construction
+        //Needs to be tested [X]
+        public double findMinY(ArrayList listing)
+        {
+            double min = 0;
+
+            min = ((dataPt)listing[0]).getY();
+
+            for (int i = 0; i < listing.Count; i++)
+            {
+                if (min > ((dataPt)listing[i]).getY())
+                {
+                    min = ((dataPt)listing[i]).getY();
+                }
+            }
+            return min;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clrDisplay_Click(object sender, EventArgs e)
+        {
+            ptDisplay.Items.Clear();
+            errTag.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           try
+            {
+                ArrayList contextualList = constructList(Convert.ToDouble(xBox.Text));
+                if (contextualList.Count == 0)
+                {
+                    solution = IDWE(Convert.ToDouble(xBox.Text), Convert.ToDouble(yBox.Text), Convert.ToDouble(tBox.Text), Convert.ToDouble(pBox.Text), dataList);
+                }
+                else
+                {
+                    solution = IDWE(Convert.ToDouble(xBox.Text), Convert.ToDouble(yBox.Text), Convert.ToDouble(tBox.Text), Convert.ToDouble(pBox.Text), contextualList);
+                }
+                ptDisplay.Items.Add("---------------------------------------------- ");
+                ptDisplay.Items.Add("The Interpolation of the Points/Values you gave is :  " + solution);
+                errTag.Text = "";
+           }
+            catch
+            {
+                errTag.Text = "Formating error, fields incorrect format!";
+                errTag.ForeColor = Color.Red;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
